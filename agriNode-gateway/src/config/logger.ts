5b12d3e23f3d972f6bf6c5edbe1f19d605a05390
larsbuecker <lars.buecker1@gmail.com>
2025-04-16 22:54:39 +0200

    Winston logs now uses colors for log levels

--- agriNode-gateway/src/config/logger.ts
+++ agriNode-gateway/src/config/logger.ts
@@ -6,13 +6,16 @@ dotenv.config();
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
   format: winston.format.combine(
+    winston.format.colorize(), // Add colorization based on log level
     winston.format.timestamp(),
-    winston.format.json()
+    winston.format.printf(({ level, message, timestamp }) => {
+      return `${timestamp} [${level}]: ${message}`;
+    })
   ),
   transports: [
     new winston.transports.Console(),
     ...(process.env.LOG_TO_FILE === 'true' ? [
-      new winston.transports.File({ filename: 'application.log' })
+      new winston.transports.File({ filename: 'application.log', format: winston.format.json() })
     ] : [])
   ],
 });
