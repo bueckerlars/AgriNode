c651e806e6609513a0e2feef52ab9c0c8293fa0a
larsbuecker <lars.buecker1@gmail.com>
2025-04-16 22:52:37 +0200

    Added Sidebartrigger on closed sidebar

--- agriNode-dashboard/src/components/AppLayout.tsx
+++ agriNode-dashboard/src/components/AppLayout.tsx
@@ -1,3 +1,4 @@
+import { SidebarProvider, SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
 import AppSidebar from "./AppSidebar";
 import { SidebarInset } from "./ui/sidebar";
 
@@ -6,13 +7,15 @@ interface AppLayoutProps {
 }
 
 export const AppLayout = ({children}: AppLayoutProps) => {
+    const { open } = useSidebar();
 
     return (
         <div className="min-h-screen flex w-full">
               <AppSidebar />
               <SidebarInset>
+                {!open && <div className="p-4"><SidebarTrigger /></div>}
                 {children}
               </SidebarInset>
         </div>
     );
-}
+};
